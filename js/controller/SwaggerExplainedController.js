042bcecb613a60197a813058e6a5b7f7900f8e71
Bob Fanger <bfanger@gmail.com>
2015-09-17 23:15:08 +0200

    Added new Annotations types.

--- js/controller/SwaggerExplainedController.js
+++ js/controller/SwaggerExplainedController.js
@@ -7,22 +7,32 @@ app.controller('SwaggerExplainedController', function ($scope, $http, swgOutputS
         post: "@SWG\\Post",
         put: "@SWG\\Put",
         patch: "@SWG\\Patch",
-        delete: "@SWG\\Delete"
+        delete: "@SWG\\Delete",
+        head: "@SWG\\Head",
+        options: "@SWG\\Options"
     };
-    function explainJsonSpec(object) {
+    function explainJsonSpec(object, type) {
         if (object.items) {
-            object.items._explained = {
-                annotation: "@SWG\\Items",
-                spec: "itemsObject"
-            };
-            explainJsonSpec(object.items);
+            if (type === "parameter" && object.in !== "body" || type === "header") {
+                object.items._explained = {
+                    annotation: "@SWG\\Items",
+                    spec: "itemsObject"
+                };
+                explainJsonSpec(object.items, "items");
+            } else {
+                object.items._explained = {
+                    annotation: "@SWG\\Items",
+                    spec: "schemaObject"
+                };
+                explainJsonSpec(object.items, "items");
+            }
         }
         if (object.schema) {
             object.schema._explained = {
                 annotation: "@SWG\\Schema",
                 spec: "schemaObject"
             };
-            explainJsonSpec(object.schema);
+            explainJsonSpec(object.schema, "schema");
         }
         if (object.properties) {
             for (var name in object.properties) {
@@ -31,7 +41,7 @@ app.controller('SwaggerExplainedController', function ($scope, $http, swgOutputS
                     annotation: "@SWG\\Property",
                     spec: "schemaObject"
                 };
-                explainJsonSpec(property);
+                explainJsonSpec(property, "property");
             }
             
         }
@@ -41,7 +51,19 @@ app.controller('SwaggerExplainedController', function ($scope, $http, swgOutputS
                 schema._explained = {
                     spec: "schemaObject"
                 };
-                explainJsonSpec(schema);
+                explainJsonSpec(schema, "schema");
+            }
+        }
+        if (object.externalDocs) {
+            object.externalDocs._explained = {
+                annotation: "@SWG\\ExternalDocumentation",
+                spec: "externalDocumentationObject"
+            }
+        }
+        if (object.xml) {
+            object.xml._explained = {
+                annotation: "@SWG\\Xml",
+                spec: "xmlObject"
             }
         }
     }
@@ -105,17 +127,27 @@ app.controller('SwaggerExplainedController', function ($scope, $http, swgOutputS
                                     spec: 'parameterObject',
                                     annotation: '@SWG\\Parameter'
                                 };
-                                explainJsonSpec(parameter);
+                                explainJsonSpec(parameter, "parameter");
                             }
                         }
                         if (operation.responses) {
                             for (var j in operation.responses) {
-                                var response =operation.responses[j];
+                                var response = operation.responses[j];
                                 response._explained = {
                                     spec: 'responseObject',
                                     annotation: '@SWG\\Response'
                                 };
-                                explainJsonSpec(response);
+                                explainJsonSpec(response, "response");
+                                if (response.headers) {
+                                    for (var k in response.headers) {
+                                        var header = response.headers[k];
+                                        header._explained = {
+                                            spec: "headerObject",
+                                            annotation: "@SWG\\Header"
+                                        }
+                                    }
+                                    explainJsonSpec(header, "header");
+                                }
                             }
                             operation.responses._explained = {
                                 spec: "responsesObject"
@@ -134,11 +166,29 @@ app.controller('SwaggerExplainedController', function ($scope, $http, swgOutputS
                         spec: "schemaObject",
                         annotation: "@SWG\\Definition"
                     };
-                    explainJsonSpec(definition);
+                    explainJsonSpec(definition, "definition");
                 }
                 swagger.definitions._explained = {
                     spec: "definitionsObject"
                 };
+            }
+            if (swagger.securityDefinitions) {
+                for (var name in swagger.securityDefinitions) {
+                    var securityDefinition = swagger.securityDefinitions[name];
+                    securityDefinition._explained = {
+                        annotation: "@SWG\\SecurityScheme",
+                        spec: "securitySchemeObject"
+                    }
+                }
+            }
+            if (swagger.tags) {
+                for (var i in swagger.tags) {
+                    var tag = swagger.tags[i];
+                    tag._explained = {
+                        annotation: "@SWG\\Tag",
+                        spec: "tagObject"
+                    }
+                }
             }
             $scope.swagger = swagger;
         }).catch(function (response) {
